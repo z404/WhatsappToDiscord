c4f8fcf854059b90b7b354cef1052e434fa51d72
z404 <anishr890@gmail.com>
2021-01-21 10:13:56 +0530

    Run both programs and it sparingly works

--- new_messages_observer2.py
+++ new_messages_observer2.py
@@ -1,12 +1,15 @@
-import os
+import os, json
 import sys
 import time
+import pyrebase
 
 from webwhatsapi import WhatsAPIDriver
 
 driver = 0
 
-    
+flag = False
+msg = ''
+
 def run():
     global driver
     driver = WhatsAPIDriver()
@@ -17,14 +20,22 @@ def run():
     driver.subscribe_new_messages(NewMessageObserver())
     print("Waiting for new messages...")
 
+    with open('config.txt') as file:
+        config = json.load(file)
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+    db.set({"hello":"hi"})
+    
     """ Locks the main thread while the subscription in running """
+    global flag, msg
     while True:
-        time.sleep(60)
+        if flag:
+            db.set({"hello":msg})
+            flag = False
 
 
 class NewMessageObserver:
 
-    
     def on_message_received(self, new_messages):
             
         for message in new_messages:
@@ -37,6 +48,9 @@ class NewMessageObserver:
                     name = str(driver.get_chat_from_id(message.chat_id))[13:].split(':')[0]
                 contact = str(driver.get_contact_from_id(message.sender.id))[9:].split('(')[0]
                 print(name,message.content,contact)
+                global msg, flag
+                msg = message.content
+                flag = True
             else:
                 print(
                     "New message of type '{}' received from number {}".format(
